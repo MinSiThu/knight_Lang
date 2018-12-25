911ef7f70e9996b4e6c59668cae0e9295d50324b
Min Si Thu <archimedes557@gmail.com>
2018-12-25 14:31:59 +0630

    new changes
    test

---
+++ runtime/reader.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+
+module.exports = function(filePath){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(filePath,(err,data)=>{
+            if(err) reject(err);
+            else resolve(data+"");
+        })
+    })
+}
